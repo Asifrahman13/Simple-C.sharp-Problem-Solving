89713096a03bb11252ffae1a7cf76710ba0eae0d
Asifrahman13 <128958139+Asifrahman13@users.noreply.github.com>
2024-03-29 06:56:05 +0600

    Add files via upload

---
+++ Improve renewal rate of subscriptions.cs
@@ -0,0 +1,39 @@
+﻿
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <=1)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} day! Renew now and save {discountPercentage}%!");
+    discountPercentage = 20;
+}
+
+
+else if (daysUntilExpiration <=5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days! Renew now and save {discountPercentage}%!");
+    discountPercentage = 10;
+}
+
+
+else if (daysUntilExpiration <=10)
+
+{
+    if (daysUntilExpiration!<=10)
+    {
+    Console.WriteLine();
+    }
+
+    else
+    { 
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+
+}
+
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
